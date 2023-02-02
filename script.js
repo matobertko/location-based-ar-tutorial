08d9546e843c3bfa1371e34c0e1da7df512d931c
Martin Bertko <514588@mail.muni.cz>
2023-02-02 23:38:51 +0100

    switching buton

--- script.js
+++ script.js
@@ -1,77 +1,37 @@
 window.onload = () => {
-    const button = document.querySelector('button[data-action="change"]');
-    button.innerText = '﹖';
-
-    let places = staticLoadPlaces();
-    renderPlaces(places);
+    // insert values to coords
+    const longitude = 21.265328; // <- insert here
+    const latitude = 48.759925;  // <- insert here
+    manageModelVisibility();
 };
 
-function staticLoadPlaces() {
-    return [
-        {
-            name: 'Pokèmon',
-            location: {
-                lat: 48.759925, 
-                lng: 21.265328,
-            },
-        },
-    ];
+function createModel(lat, long) {
+    let model = document.createElement('a-entity');
+    model.setAttribute('gltf-model', './minimickey.glb');
+    model.setAttribute('scale', `2 2 2`);
+    model.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long};`);
+
+    return model;
 }
 
-var models = [
-    {
-        url: './church.glb',
-        scale: '0.5 0.5 0.5',
-        info: 'church',
-        rotation: '0 0 0',
-    },
-    {
-        url: './minimickey.glb',
-        scale: '2 2 2',
-        rotation: '0 0 0',
-        info: 'minimickey',
-    },
-];
-
-var modelIndex = 0;
-var setModel = function (model, entity) {
-    if (model.scale) {
-        entity.setAttribute('scale', model.scale);
-    }
-
-    if (model.rotation) {
-        entity.setAttribute('rotation', model.rotation);
-    }
-
-    if (model.position) {
-        entity.setAttribute('position', model.position);
-    }
-
-    entity.setAttribute('gltf-model', model.url);
-
-    const div = document.querySelector('.instructions');
-    div.innerText = model.info;
-};
-
-function renderPlaces(places) {
+function manageModelVisibility(lat, long) {
     let scene = document.querySelector('a-scene');
-
-    places.forEach((place) => {
-        let latitude = place.location.lat;
-        let longitude = place.location.lng;
-
-        let model = document.createElement('a-entity');
-        model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
-
-        setModel(models[modelIndex], model);
-
-        document.querySelector('button[data-action="change"]').addEventListener('click', function () {
-            var entity = document.querySelector('[gps-entity-place]');
-            modelIndex++;
-            var newIndex = modelIndex % models.length;
-            setModel(models[newIndex], entity);
-        });
-
-        scene.appendChild(model);
+    let button = document.querySelector('button[data-action="change"]')
+    
+    let withInfos = false;
+    let model = createModel(lat, long);
+    button.addEventListener('click', function(e) {
+        // console.log("stlaceni");
+        if (withInfos === false) {
+            console.log("false");
+            scene.appendChild(model);
+            withInfos = true;
+        } else {
+            console.log("true");
+            scene.removeChild(model);
+            let newModel = createModel(lat, long);
+            model = newModel;
+            withInfos = false;
+        }
     });
 }
