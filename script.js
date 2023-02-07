84f31f9f00e9211b17686e7badfbd529f2844bfb
Martin Bertko <xbertko1@fi.muni.cz>
2023-02-07 01:35:20 +0100

    reinvention of showing signs

--- script.js
+++ script.js
@@ -1,44 +1,18 @@
 window.onload = () => {
-    // insert values to coords
-    const longitude = 21.265328; // <- insert here
-    const latitude = 48.759925;  // <- insert here
-    manageModelVisibility(latitude, longitude);
-};
-
-function createModel(lat, long) {
-    // let model = document.createElement('a-entity');
-    // model.setAttribute('gltf-model', './minimickey.glb');
-    // model.setAttribute('scale', `2 2 2`);
-    // model.setAttribute('position', `0 10 0`);
-    // model.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long};`);
-    let model = document.createElement('a-entity');
-    model.setAttribute('gltf-model', './sign_fontana.glb');
-    model.setAttribute('scale', "1 1 1");
-    entity.setAttribute("look-at", "[camera]");
-    // model.setAttribute('rotation', `0 90 0`);
-    model.setAttribute('gps-entity-place', 'latitude: ${lat}; longitude: ${long};');
-
-    return model;
-}
-
-function manageModelVisibility(lat, long) {
     let scene = document.querySelector('a-scene');
     let button = document.querySelector('button[data-action="change"]')
-    
-    let withInfos = false;
-    let model = createModel(lat, long);
+
+    let showSigns = false;
     button.addEventListener('click', function(e) {
-        // console.log("stlaceni");
-        if (withInfos === false) {
-            console.log("false");
-            scene.appendChild(model);
-            withInfos = true;
-        } else {
-            console.log("true");
-            scene.removeChild(model);
-            let newModel = createModel(lat, long);
-            model = newModel;
-            withInfos = false;
-        }
+        showSigns = !showSigns;
+        manageModelVisibility(showSigns);
     });
+};
+
+
+function manageModelVisibility(showSigns) {
+    var signEntities = scene.querySelectorAll('.signs');
+    for (var i = 0; i < signEntities.length; i++) {
+        signEntities[i].setAttribute("visible", showSigns.toString());
+    }
 }
