64986d57b18307392ceab975f7885e78796ad971
Martin Bertko <xbertko1@fi.muni.cz>
2023-02-07 01:44:58 +0100

    few changes

--- script.js
+++ script.js
@@ -1,16 +1,17 @@
 window.onload = () => {
     let scene = document.querySelector('a-scene');
-    let button = document.querySelector('button[data-action="change"]')
 
     let showSigns = false;
+    
+    let button = document.querySelector('button[data-action="change"]')
     button.addEventListener('click', function(e) {
         showSigns = !showSigns;
-        manageModelVisibility(showSigns);
+        manageModelsVisibility(scene, showSigns);
     });
 };
 
 
-function manageModelVisibility(showSigns) {
+function manageModelsVisibility(scene, showSigns) {
     var signEntities = scene.querySelectorAll('.signs');
     for (var i = 0; i < signEntities.length; i++) {
         signEntities[i].setAttribute("visible", showSigns.toString());
