2622b4617f0d8343e7abf0e50896aca4935e9d94
Martin Bertko <xbertko1@fi.muni.cz>
2023-02-07 12:37:58 +0100

    preformatted for js inserting entities 2

--- script.js
+++ script.js
@@ -1,10 +1,17 @@
 window.onload = () => {
     let scene = document.querySelector('a-scene');
 
-    createSigns(scene, 8);
+    // ---------- coords ----------
+    // kounicova> 49.210930, 16.594155
+    let latitude = 49.210930;
+    let longitude = 16.594155; 
 
-    let showSigns = false;
+    // --- create entities --------
+    createCrossroad(scene, latitude, longitude);
+    createSigns(scene, 8, latitude, longitude);
     
+    // ----- manage signs --------
+    let showSigns = false;
     let button = document.querySelector('button[data-action="change"]')
     button.addEventListener('click', function(e) {
         showSigns = !showSigns;
@@ -13,21 +20,27 @@ window.onload = () => {
 };
 
 
-function createSigns(scene, countOfSigns) {
-    // ---------- coords ----------
-    let latitude = 49.210930;
-    let longitude = 16.594155;
-    // kounicova> 49.210930, 16.594155
+function createCrossroad(scene, lat, long) {
+    // create entity
+    let crossroadEntity = document.createElement("a-entity");
+    crossroadEntity.setAttribute('class', 'crossroad');
+    crossroadEntity.setAttribute('gltf-model', './assets/model_krizovatky3.glb');
+    crossroadEntity.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long};`);
+    
+    // insert entity into the scene
+    scene.appendChild(crossroadEntity);
+}
 
-    // ------ new entity ----------
+
+function createSigns(scene, countOfSigns, lat, long) {
     for (let objIdx = 1; objIdx <= countOfSigns; objIdx++) {
         // create entity
         let signEntity = document.createElement("a-entity");
         signEntity.setAttribute('class', 'sign');
-        signEntity.setAttribute('gltf-model', `./assets/sign${objIdx}.glb`);
+        signEntity.setAttribute('gltf-model', './assets/sign${objIdx}.glb');
         signEntity.setAttribute('visible', 'false');
         signEntity.setAttribute('look-at', '[gps-camera]');
-        signEntity.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+        signEntity.setAttribute('gps-entity-place', `latitude: ${lat}; longitude: ${long};`);
 
         // insert entity into the scene
         scene.appendChild(signEntity);
