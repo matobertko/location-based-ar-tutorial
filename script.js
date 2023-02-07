076fa742ce936bc3146823903ce6917fd0273c6a
Martin Bertko <xbertko1@fi.muni.cz>
2023-02-07 11:04:32 +0100

    test of js

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 window.onload = () => {
-    let scene = document.querySelector('a-scene');
+    var scene = document.querySelector('a-scene');
 
     let showSigns = false;
     
@@ -11,8 +11,28 @@ window.onload = () => {
 };
 
 
-function manageModelsVisibility(scene, showSigns) {
-    var signEntities = scene.querySelectorAll('.signs');
+// function createSignEntity(objIdx) {
+//     // ------ coords ----------
+//     let latitude = 49.210930;
+//     let longitude = 16.594155;
+//     // kounicova> 49.210930, 16.594155
+
+//     // ------ new entity ----------
+//     // class="signs"
+//     // gltf-model="./assets/sign6.glb" 
+//     // visible="false"
+//     // look-at="[gps-camera]"
+//     // gps-entity-place="longitude: 21.265328; latitude: 48.759925;" 
+//     let entity = document.createElement("a-entity");
+    
+//     model.setAttribute('gps-entity-place', `latitude: ${latitude}; longitude: ${longitude};`);
+
+//     scene.appendChild(entity);
+// }
+
+
+function manageSignsVisibility(showSigns) {
+    var signEntities = scene.querySelectorAll('.sign');
     for (var i = 0; i < signEntities.length; i++) {
         signEntities[i].setAttribute("visible", showSigns.toString());
     }
