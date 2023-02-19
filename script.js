8671087551faadc1d9147114294c8e594e8b6b24
Martin Bertko <xbertko1@fi.muni.cz>
2023-02-19 19:11:21 +0100

    test of new atr after added semicolons

--- script.js
+++ script.js
@@ -30,8 +30,8 @@ function createCrossroad(scene, lat, long) {
     crossroadEntity.setAttribute('class', 'crossroad');
     crossroadEntity.setAttribute('gltf-model', './assets/model_krizovatky3.glb');
     // crossroadEntity.setAttribute('look-at', '[gps-camera]');
-    // crossroadEntity.setAttribute('gps-new-entity-place', `latitude: ${lat}; longitude: ${long}`);
-    crossroadEntity.setAttribute('gps-projected-entity-place', `latitude: ${lat}; longitude: ${long};`);
+    crossroadEntity.setAttribute('gps-new-entity-place', `latitude: ${lat}; longitude: ${long};`);
+    // crossroadEntity.setAttribute('gps-projected-entity-place', `latitude: ${lat}; longitude: ${long};`);
 
     
     // insert entity into the scene
@@ -49,8 +49,8 @@ function createSigns(scene, countOfSigns, lat, long) {
         signEntity.setAttribute('gltf-model', `./assets/sign${objIdx}.glb`);
         signEntity.setAttribute('visible', 'false');
         // signEntity.setAttribute('look-at', '[gps-camera]');
-        // signEntity.setAttribute('gps-new-entity-place', `latitude: ${lat}; longitude: ${long}`);
-        signEntity.setAttribute('gps-projected-entity-place', `latitude: ${lat}; longitude: ${long};`);
+        signEntity.setAttribute('gps-new-entity-place', `latitude: ${lat}; longitude: ${long};`);
+        // signEntity.setAttribute('gps-projected-entity-place', `latitude: ${lat}; longitude: ${long};`);
 
 
         // insert entity into the scene
